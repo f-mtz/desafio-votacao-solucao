0c8f83c6f38d08763df385a6fa31aba17dbd6d14
f-mtz <mtz.dev.si@gmail.com>
2025-11-24 22:41:19 -0300

    feat(repository): Listando agenda por status IN_PROGRESS

--- api/src/main/java/sicredi/votacao/api/repository/AgendaRepository.java
+++ api/src/main/java/sicredi/votacao/api/repository/AgendaRepository.java
@@ -3,9 +3,13 @@ package sicredi.votacao.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sicredi.votacao.api.domain.Agenda;
+import sicredi.votacao.api.domain.AgendaStatus;
+
+import java.util.List;
 
 @Repository
 public interface AgendaRepository extends JpaRepository<Agenda, Long> {
+	List<Agenda> findByStatus(AgendaStatus status);
 }
 
 
